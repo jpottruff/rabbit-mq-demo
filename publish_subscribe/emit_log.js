867c1d3e4264700641c3bf3a4002b7430d519a55
Jeff Pottruff <jeffpottruff@gmail.com>
2025-01-22 20:20:42 -0500

    add assert / publish to exchange logic

--- publish_subscribe/emit_log.js
+++ publish_subscribe/emit_log.js
@@ -17,6 +17,17 @@ amqp.connect(SERVER_URL, function (error0, connection) {
     var exchange = TEST_EXCHANGE;
     /** Allows arbitrary messages to be sent from command line */
     var msg = process.argv.slice(2).join(" ") || "Hello World!";
+
+    // Specify to publish messages to TEST_EXCHANGE instead of the default nameless one
+    // NOTE: necessary when working with named exchanges as they must exist
+    channel.assertExchange(exchange, "fanout", {
+      durable: false,
+    });
+
+    // The second paramter ("") means we don't want to send the message to any specific queue - just publish it to the TEST_EXCHANGE
+    channel.publish(exchange, "", Buffer.from(msg));
+
+    console.log(" [x] Sent %s", msg);
   });
 
   // Close connection and exit
