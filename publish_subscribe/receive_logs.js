da5f166cb1044464bda1623459c3ad13084a35cf
Jeff Pottruff <jeffpottruff@gmail.com>
2025-01-22 20:21:04 -0500

    add subscriber logic

--- publish_subscribe/receive_logs.js
+++ publish_subscribe/receive_logs.js
@@ -15,5 +15,46 @@ amqp.connect(SERVER_URL, function (error0, connection) {
 
     // Set up a named exchange
     var exchange = TEST_EXCHANGE;
+
+    channel.assertExchange(exchange, "fanout", {
+      durable: false,
+    });
+
+    // Generate a queue with a random name EXAMPLE `amq.gen-JzTY20BRgKO-HjmUJj0wLg`
+    // NOTE: this queue will be deleted when the connection that declared it closes
+    channel.assertQueue(
+      // queue name (random)
+      "",
+      // options
+      {
+        exclusive: true,
+      },
+      // callback
+      function (error2, q) {
+        if (error2) {
+          throw error2;
+        }
+        console.log(
+          " [*] Waiting for messages in %s. To exit press CTRL+C",
+          q.queue
+        );
+
+        // bind the queue to the exchange
+        channel.bindQueue(q.queue, exchange, "");
+
+        // consume messages
+        channel.consume(
+          q.queue,
+          function (msg) {
+            if (msg.content) {
+              console.log(" [x] %s", msg.content.toString());
+            }
+          },
+          {
+            noAck: true,
+          }
+        );
+      }
+    );
   });
 });
