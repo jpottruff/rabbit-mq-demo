43f7a9856fc501ee0d1dd40ed6ff47a73cbbf4da
Jeff Pottruff <jeffpottruff@gmail.com>
2025-05-01 13:04:58 -0400

    add rpc client

--- remote-procedure-call/client.js
+++ remote-procedure-call/client.js
@@ -1,6 +1,13 @@
 const amqp = require("amqplib/callback_api");
 const { SERVER_URL, RPC_QUEUE } = require("./config");
 
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+  console.log("Usage: rpc_client.js <num>");
+  process.exit(1);
+}
+
 // Connect to server
 amqp.connect(SERVER_URL, function (error0, connection) {
   if (error0) {
@@ -15,23 +22,49 @@ amqp.connect(SERVER_URL, function (error0, connection) {
 
     // Setup and send message to queue
     var queue = RPC_QUEUE;
-    /** Allows arbitrary messages to be sent from command line */
-    var msg = process.argv.slice(2).join(" ") || "Hello World!";
 
-    channel.assertQueue(queue, {
-      durable: true,
-    });
+    channel.assertQueue(
+      "",
+      {
+        exclusive: true,
+      },
+      function (error2, q) {
+        if (error2) {
+          throw error2;
+        }
 
-    channel.sendToQueue(queue, Buffer.from(msg), {
-      persistent: true,
-    });
+        var correlationId = generateUuid();
+        var num = parseInt(args[0]);
+        console.log(" [x] Requesting fib(%d)", num);
 
-    console.log(" [x] Sent %s", msg);
+        channel.consume(
+          q.queue,
+          function (msg) {
+            if (msg.properties.correlationId == correlationId) {
+              console.log(" [.] Got %s", msg.content.toString());
+              setTimeout(function () {
+                connection.close();
+                process.exit(0);
+              }, 500);
+            }
+          },
+          { noAck: true }
+        );
 
-    // Close connection and exit
-    setTimeout(function () {
-      connection.close();
-      process.exit(0);
-    }, 500);
+        channel.sendToQueue(queue, Buffer.from(num.toString()), {
+          correlationId: correlationId,
+          replyTo: q.queue,
+        });
+      }
+    );
   });
 });
+
+/** Not foolproof but will do */
+function generateUuid() {
+  return (
+    Math.random().toString() +
+    Math.random().toString() +
+    Math.random().toString()
+  );
+}
